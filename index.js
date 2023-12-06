8921d8ffd8337fbfb03fec8b4fac2ddb80d3cce6
coffee-converter <coffee.becomes.code@gmail.com>
2023-12-06 15:43:25 -0600

    use opensea sdk api

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { program } = require('commander');
 require('dotenv').config();
 const { fetch } = require('cross-fetch');
+const { OpenSeaAPI, Chain } = require("opensea-js");
+
+const openseaAPI = new OpenSeaAPI({
+  chain: Chain.Mainnet,
+  apiKey: process.env.OPENSEA_API_KEY,
+});
+
+const OS_API_RETRIES = 10;
 
 program
   .requiredOption('-a, --address <contract_address>')
@@ -19,13 +27,16 @@ const MARKETS = [
   {
     name: "OpenSea",
     msDelay: 400,
-    url: (address, tid) => `https://api.opensea.io/api/v1/asset/${address}/${tid}/?force_update=true`,
+    refreshAction: async (contract, tokenId, retries = OS_API_RETRIES) => {
+      await openseaAPI.refreshNFTMetadata(Chain.Mainnet, contract, tokenId.toString(), retries);
+    },
+    /*url: (address, tid) => `https://api.opensea.io/api/v1/asset/${address}/${tid}/?force_update=true`,
     fetchOpts: (address, tid) => ({
       cache: 'reload',
       headers: {
         'X-API-KEY': process.env.OPENSEA_API_KEY,
       },
-    }),
+    }),*/
   },
 ];
 
@@ -44,21 +55,28 @@ async function refreshMarket (market) {
   while (tokenIds.size > 0) {
     try {
       const [tid] = tokenIds;
-      const url = market.url(ADDRESS, tid);
-      const opts = market.fetchOpts(ADDRESS, tid);
 
       const timeToWait = market.msDelay - Date.now() + (lastFetchTime || 0);
       if (timeToWait > 0) await new Promise(r => setTimeout(r, timeToWait));
       lastFetchTime = Date.now();
 
-      const res = await fetch(url, opts);
-
-      if (res?.status === 200) tokenIds.delete(tid);
-      else if (res?.status === 404) {
-        tokenIds.delete(tid);
-        console.log(`\ntid ${tid} - HTTP 404`);
+      if (market.refreshAction) {
+        await market.refreshAction(ADDRESS, tid, 1);
+        tokenIds.delete(tid)
       }
-      else throw new Error(`${res?.status} ${res?.statusText}`);
+      else {
+        const url = market.url(ADDRESS, tid);
+        const opts = market.fetchOpts(ADDRESS, tid);
+
+        const res = await fetch(url, opts);
+
+        if (res?.status === 200) tokenIds.delete(tid);
+        else if (res?.status === 404) {
+          tokenIds.delete(tid);
+          console.log(`\ntid ${tid} - HTTP 404`);
+        }
+        else throw new Error(`${res?.status} ${res?.statusText}`);
+      }
 
       process.stdout.clearLine(-1);
       process.stdout.cursorTo(0);
